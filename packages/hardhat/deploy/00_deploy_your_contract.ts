d3c93ce976441ae80b402fbde64764ba82e2ab1c
Gaetano Mondelli <gaetano.mondelli@gmail.com>
2024-03-09 01:44:30 +0000

    fix: annoying error with network called localhost instead of hardhat in ethers

--- packages/hardhat/deploy/00_deploy_your_contract.ts
+++ packages/hardhat/deploy/00_deploy_your_contract.ts
@@ -19,22 +19,19 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  await deploy("YourContract", {
-    from: deployer,
-    // Contract constructor arguments
-    args: [deployer],
-    log: true,
-    // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
-    // automatically mining the contract deployment transaction. There is no effect on live networks.
-    autoMine: true,
-  });
 
   let contractRegistry: Contract;
   let ftsoRegistry: Contract;
   let broker: Contract;
 
+  // address private FLARE_CONTRACT_REGISTRY =
+  // 0xaD67FE66660Fb8dFE9d6b1b4240d8650e30F6019;
 
-  if (hre.network.name === "hardhat") {
+  let contractRegistryAddress = "0xaD67FE66660Fb8dFE9d6b1b4240d8650e30F6019";
+
+
+  if (hre.network.name === "localhost") {
+    // Deploy the Mock contracts
     await deploy("MockContractRegistry", {
       from: deployer,
       args: [],
@@ -52,23 +49,22 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
     });
 
     ftsoRegistry = await hre.ethers.getContract<Contract>("MockFtsoRegistry", deployer);
+    contractRegistryAddress = await contractRegistry.getAddress();
     contractRegistry.setContractAddress("FtsoRegistry", await ftsoRegistry.getAddress());
   }
 
-  await deploy("MockFtsoRegistry", {
+  await deploy("Broker", {
     from: deployer,
-    args: [await contractRegistry.getAddress(), "C2FLR", collateralRatio, lockinPeriod, fee],
+    args: [contractRegistryAddress, "C2FLR", collateralRatio, lockinPeriod, fee],
     log: true,
     autoMine: true,
   });
 
   // Get the deployed contract to interact with it after deploying.
-  const yourContract = await hre.ethers.getContract<Contract>("YourContract", deployer);
-  console.log("👋 Initial greeting:", await yourContract.greeting());
 };
 
 export default deployYourContract;
 
 // Tags are useful if you have multiple deploy files and only want to run one of them.
 // e.g. yarn deploy --tags YourContract
-deployYourContract.tags = ["YourContract"];
+// deployYourContract.tags = ["YourContract"];
