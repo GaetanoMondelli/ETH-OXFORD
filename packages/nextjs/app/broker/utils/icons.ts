a8b65288c787cce2975d87393e2449fb5467d552
Gaetano Mondelli <gaetano.mondelli@gmail.com>
2024-03-09 13:02:07 +0000

    feat: added deployments and icons

---
+++ packages/nextjs/app/broker/utils/icons.ts
@@ -0,0 +1,6 @@
+export const icons = {
+  btc: "https://assets.coingecko.com/coins/images/1/standard/bitcoin.png?1696501400",
+  eth: "https://assets.coingecko.com/coins/images/279/standard/ethereum.png?1595348880",
+  flare: "https://assets.coingecko.com/coins/images/28624/standard/FLR-icon200x200.png?1696527609",
+  sp500: "https://investment-s3.unitedtraders.io/shares/SPY.svg",
+};
