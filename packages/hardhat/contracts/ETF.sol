d36ab52cc672a81330ca3f7aa1040573b34165db
Gaetano Mondelli <gaetano.mondelli@gmail.com>
2024-03-09 15:42:19 +0000

    feat: added mint check tokens required and first iteration of otherchain transaction proof

--- packages/hardhat/contracts/ETF.sol
+++ packages/hardhat/contracts/ETF.sol
@@ -4,16 +4,25 @@ pragma solidity ^0.8.0;
 
 import { CollateralVault } from "./Collateral.sol";
 import { IERC721 } from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {EVMTransaction} from "@flarenetwork/flare-periphery-contracts/coston/stateConnector/interface/EVMTransaction.sol";
+import {FlareContractsRegistryLibrary} from "@flarenetwork/flare-periphery-contracts/coston/util-contracts/ContractRegistryLibrary.sol";
 
 struct Token {
     address _address;
     uint256 _quantity;
     uint256 _chainId;
+    address _contributor;
+}
+
+struct Vault {
+    Token[] _tokens;
+    VaultState state;
 }
 
 enum VaultState {
     EMPTY,
+    OPEN,
     MINTED,
     BURNED
 }
@@ -21,12 +30,27 @@ enum VaultState {
 contract ETF  {
 	address public flareContractRegistry;
     Token[] requiredTokens;
-    mapping(uint256 => VaultState) public vaults;
+    uint256 public chainId;
+    
 
-    constructor(address _flareContractRegistry) {
+    mapping(uint256 => Vault) public vaults;
+    
+    struct EventInfo {
+    address sender;
+    uint256 value;
+    bytes data;
+}
+
+struct TransactionInfo {
+    EVMTransaction.Proof originalTransaction;
+    uint256 eventNumber;
+    EventInfo[] eventInfo;
+}
+
+    constructor(address _flareContractRegistry, uint256 _chainId) {
         flareContractRegistry = _flareContractRegistry;
+        chainId = _chainId;
     }
-
 
     function getBundleStates()
         public
@@ -35,20 +59,100 @@ contract ETF  {
     {
         VaultState[] memory states = new VaultState[](90);
         for (uint256 i = 0; i < 90; i++) {
-            states[i] = vaults[i];
+            states[i] = vaults[i].state;
         }
         return states;
     }
 
 
-    function getBundle(uint256 bundleId) public view returns (address[] memory, uint256[] memory) {
-        address[] memory addresses = bundleIdToAddress[bundleId];
-        uint256[] memory quantities = new uint256[](addresses.length);
-        for (uint256 i = 0; i < addresses.length; i++) {
-            quantities[i] = tokenQuantities[bundleId][addresses[i]];
+    function checkVaultCompletion(uint256 _vaultId) public view returns (bool) {
+        for (uint256 i = 0; i < requiredTokens.length; i++) {
+            bool found = false;
+            for (uint256 j = 0; j < vaults[_vaultId]._tokens.length; j++) {
+                if (requiredTokens[i]._address == vaults[_vaultId]._tokens[j]._address) {
+                    found = true;
+                    // check if the quantity is enough
+                    if (requiredTokens[i]._quantity > vaults[_vaultId]._tokens[j]._quantity) {
+                        return false;
+                    }
+                    break;
+                }
+            }
+            if (!found) {
+                return false;
+            }
         }
-        return (addresses, quantities);
+        return true;
     }
 
+    function deposit(uint256 _vaultId, Token[] memory _tokens) public {
+        require(vaults[_vaultId].state == VaultState.OPEN, "Vault is not open");
+        uint256 whitelistedQuantity = 0;
+        for (uint256 i = 0; i < _tokens.length; i++) {
+            uint256 whitelistedIndex = requiredTokens.length;
+            for (uint256 j = 0; j < requiredTokens.length; j++) {
+                if (requiredTokens[j]._address == _tokens[i]._address) {
+                    whitelistedIndex = j;
+                    break;
+                }
+            }
+            require(
+                whitelistedIndex < requiredTokens.length,
+                "Token not allowed"
+            );
+            require(
+                _tokens[i]._quantity >= requiredTokens[whitelistedIndex]._quantity,
+                "Insufficient quantity"
+            );
+            whitelistedQuantity++;
+        }
+        require(
+            whitelistedQuantity == requiredTokens.length,
+            "Some tokens are missing"
+        );
+        
+        // transfer tokens to the vault
+        for (uint256 i = 0; i < _tokens.length; i++) {
+            IERC20(_tokens[i]._address).transferFrom(
+                _tokens[i]._contributor,
+                address(this),
+                _tokens[i]._quantity
+            );
+        }
 
+        if (checkVaultCompletion(_vaultId)) {
+            vaults[_vaultId].state = VaultState.MINTED;
+        }
+    }
+
+    TransactionInfo[] public transactions;
+
+    function collectPayment(EVMTransaction.Proof calldata _transaction) external {
+        require(isEVMTransactionProofValid(_transaction), "Invalid transaction proof");
+
+        uint256 transactionIndex = transactions.length;
+        transactions.push();
+        transactions[transactionIndex].originalTransaction = _transaction;
+        transactions[transactionIndex].eventNumber = _transaction.data.responseBody.events.length;
+        EventInfo[] storage eventInfo = transactions[transactionIndex].eventInfo;
+        for(uint256 i = 0; i < _transaction.data.responseBody.events.length; i++) {
+
+            // Decode each event
+            (address sender, uint256 value, bytes memory data) = abi.decode(_transaction.data.responseBody.events[i].data, (address, uint256, bytes));
+            eventInfo.push(EventInfo({
+                sender:sender,
+                value:value,
+                data:data
+            }));
+
+        }
+    }
+        function isEVMTransactionProofValid(
+        EVMTransaction.Proof calldata transaction
+    ) public view returns (bool) {
+        // Use the library to get the verifier contract and verify that this transaction was proved by state connector
+        return FlareContractsRegistryLibrary
+                .auxiliaryGetIEVMTransactionVerification()
+                .verifyEVMTransaction(transaction);
+    }
 }
